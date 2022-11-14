fb18b4d6d17d000e1e5bb58bde23c6db8e9926aa
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2022-11-14 10:34:02 -0300

    Sidebar ChatList (1/2) 10:57

--- whatsappclone/src/components/ChatListItem.js
+++ whatsappclone/src/components/ChatListItem.js
@@ -3,6 +3,18 @@ import './ChatListItem.css';
 
 export default () => {
     return (
-        <div>...</div>
+        <div className='chatListItem'>
+            <img className='charListItem--avatar' src="https://www.w3schools.com/howto/img_avatar2.png" alt='' />
+            <div className='charListItem--lines'>
+                <div className='charListItem--line'>
+                    <div className='charListItem--name'>Daniel Rocha</div>
+                    <div className='charListItem--date'>19:00</div>
+                </div>
+                <div className='charListItem--line'>
+                    <div className='charListItem--lastMsg'><p>Opa, tudo bem?</p></div>
+                </div>
+            </div>
+
+        </div>
     );
 }
